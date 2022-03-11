bae24646f6266446e87c3290a1b1dcd8ffd360f0
UserRF <you@example.com>
2022-03-11 15:12:48 +0300

    1

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,24 @@
 'use strict';
 const footerNav = document.querySelector('.footer__nav')
 const footerNavNoJS = document.querySelector('.footer__nav--nojs')
+const footerNavClosed = document.querySelector('.footer__nav--closed')
+const footerNavOpened = document.querySelector('.footer__nav--opened')
+const btnNavFooter = document.getElementById('footerBtnNav');
 
 footerNav.classList.remove(footerNavNoJS)
+
+btnNavFooter.addEventListener('click', ()=> {
+  if (footerNavClosed) {
+    footerNav.classList.add(footerNavOpened)
+    footerNav.remove(footerNavClosed)
+    console.log(1)
+  } else {
+    footerNav.remove(footerNavOpened)
+    footerNav.classList.add(footerNavClosed)
+    console.log(2)
+  }
+})
+
 
 
 // Form
