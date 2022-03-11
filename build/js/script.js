656b14215a4c8021c497e095911a8aae5bb7d6d8
UserRF <you@example.com>
2022-03-11 11:57:56 +0300

    1

--- build/js/script.js
+++ build/js/script.js
@@ -1,4 +1,38 @@
 'use strict';
+const btnAccardionNav = document.getElementById('footerBtnNav');
+const footernojsNav = document.querySelector('.footer__nav--nojs');
+const footerNavWrapper = document.querySelector('.footer__list-wrapper');
+const footerNavOpened = document.querySelector('.footer__nav--opened');
+const footerNavClosed = document.querySelector('.footer__nav--closed');
+
+const btnAccardionContacts = document.getElementById('footerBtnNav');
+const footernojsContacts = document.querySelector('.footer__contacts--nojs');
+const footerContactsWrapper = document.querySelector('.footer__contacts-wrapper');
+const footerContactsOpened = document.querySelector('.footer__contacts--opened');
+const footerContactsClosed = document.querySelector('.footer__contacts--closed');
+
+// footerNavWrapper.remove(footernojsNav);
+// footerContactsWrapper.remove(footernojsContacts);
+
+
+btnAccardionNav.addEventListener('click', ()=>{
+  if(footerNavWrapper.classList.contains('.footer__contacts--closed')) {
+    footerNavWrapper.classList.add('.footer__contacts--opened');
+  } else {
+    footerNavWrapper.remove('.footer__contacts--opened')
+    footerNavWrapper.classList.add('.footer__contacts--closed');
+  }
+})
+
+// btnAccardionContacts.addEventListener('click', ()=>{
+//   if(footerContactsWrapper.contains(footerContactsClosed)) {
+//     footerContactsWrapper.classList.add(footerContactsOpened);
+//   } else {
+//     footerContactsWrapper.classList.remove(footerContactsOpened)
+//     footerContactsWrapper.classList.add(footerContactsClosed);
+//   }
+// })
+
 // Form
 const anchors = document.querySelectorAll('a[href*="#"]')
 
--- source/js/script.js
+++ source/js/script.js
@@ -3,23 +3,35 @@ const btnAccardionNav = document.getElementById('footerBtnNav');
 const footernojsNav = document.querySelector('.footer__nav--nojs');
 const footerNavWrapper = document.querySelector('.footer__list-wrapper');
 const footerNavOpened = document.querySelector('.footer__nav--opened');
+const footerNavClosed = document.querySelector('.footer__nav--closed');
 
 const btnAccardionContacts = document.getElementById('footerBtnNav');
 const footernojsContacts = document.querySelector('.footer__contacts--nojs');
 const footerContactsWrapper = document.querySelector('.footer__contacts-wrapper');
 const footerContactsOpened = document.querySelector('.footer__contacts--opened');
+const footerContactsClosed = document.querySelector('.footer__contacts--closed');
 
-btnAccardionContacts.addEventListener('click', ()=>{
-  footerContactsWrapper.remove(footernojsContacts);
-  footerContactsWrapper.toggle(footerContactsOpened);
-})
+// footerNavWrapper.remove(footernojsNav);
+// footerContactsWrapper.remove(footernojsContacts);
 
 
 btnAccardionNav.addEventListener('click', ()=>{
-  footerNavWrapper.remove(footernojsNav);
-  footerNavWrapper.toggle(footerNavOpened);
+  if(footerNavWrapper.classList.contains('.footer__contacts--closed')) {
+    footerNavWrapper.classList.add('.footer__contacts--opened');
+  } else {
+    footerNavWrapper.remove('.footer__contacts--opened')
+    footerNavWrapper.classList.add('.footer__contacts--closed');
+  }
 })
 
+// btnAccardionContacts.addEventListener('click', ()=>{
+//   if(footerContactsWrapper.contains(footerContactsClosed)) {
+//     footerContactsWrapper.classList.add(footerContactsOpened);
+//   } else {
+//     footerContactsWrapper.classList.remove(footerContactsOpened)
+//     footerContactsWrapper.classList.add(footerContactsClosed);
+//   }
+// })
 
 // Form
 const anchors = document.querySelectorAll('a[href*="#"]')
