84947d3b485e3be936868cafe0eef0cabd696e12
UserRF <you@example.com>
2022-03-11 11:00:44 +0300

    1

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,26 @@
 'use strict';
+const btnAccardionNav = document.getElementById('footerBtnNav');
+const footernojsNav = document.querySelector('.footer__nav--nojs');
+const footerNavWrapper = document.querySelector('.footer__list-wrapper');
+const footerNavOpened = document.querySelector('.footer__nav--opened');
+
+const btnAccardionContacts = document.getElementById('footerBtnNav');
+const footernojsContacts = document.querySelector('.footer__contacts--nojs');
+const footerContactsWrapper = document.querySelector('.footer__contacts-wrapper');
+const footerContactsOpened = document.querySelector('.footer__contacts--opened');
+
+btnAccardionContacts.addEventListener('click', ()=>{
+  footerContactsWrapper.remove(footernojsContacts);
+  footerContactsWrapper.toggle(footerContactsOpened);
+})
+
+
+btnAccardionNav.addEventListener('click', ()=>{
+  footerNavWrapper.remove(footernojsNav);
+  footerNavWrapper.toggle(footerNavOpened);
+})
+
+
 // Form
 const anchors = document.querySelectorAll('a[href*="#"]')
 
