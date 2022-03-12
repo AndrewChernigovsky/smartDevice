e2d8cc30ccd031793d33e68692b4f4c2469c3e5e
UserRF <you@example.com>
2022-03-12 11:39:41 +0300

    1

--- source/js/script.js
+++ source/js/script.js
@@ -9,30 +9,33 @@ footerNav.classList.remove('footer__nav--nojs')
 footerContacts.classList.remove('footer__contacts--nojs')
 
 btnNavFooter.addEventListener('click', ()=> {
+    footerContacts.classList.remove('footer__contacts--opened')
+    footerContacts.classList.add('footer__contacts--closed')
+
   if (footerNav.classList.contains('footer__nav--closed')) {
     footerNav.classList.add('footer__nav--opened')
     footerNav.classList.remove('footer__nav--closed')
-  } else {
-    footerNav.classList.remove('footer__nav--opened')
-    footerNav.classList.add('footer__nav--closed')
-  }
-  if (footerContacts.classList.contains('footer__nav--opened')) {
-    footerNav.classList.remove('footer__nav--opened')
-    footerNav.classList.add('footer__nav--closed')
-  }
+  } else if (footerContacts.classList.contains('footer__contacts--opened')) {
+    footerContacts.classList.remove('footer__contacts--opened')
+    footerContacts.classList.add('footer__contacts--closed')
+    } else {
+        footerNav.classList.remove('footer__nav--opened')
+        footerNav.classList.add('footer__nav--closed')
+    }
 })
 
 btnContactsFooter.addEventListener('click', ()=> {
+    footerNav.classList.remove('footer__nav--opened')
+    footerNav.classList.add('footer__nav--closed')
   if (footerContacts.classList.contains('footer__contacts--closed')) {
     footerContacts.classList.add('footer__contacts--opened')
     footerContacts.classList.remove('footer__contacts--closed')
-  } else {
-    footerContacts.classList.remove('footer__contacts--opened')
-    footerContacts.classList.add('footer__contacts--closed')
-  }
-  if (footerNav.classList.contains('footer__nav--opened')) {
-      footerContacts.classList.remove('footer__contacts--closed')
-      footerContacts.classList.add('footer__contacts--closed')
+  } else if (footerNav.classList.contains('footer__nav--opened')) {
+    footerNav.classList.remove('footer__nav--opened')
+    footerNav.classList.add('footer__nav--closed')
+    } else {
+        footerContacts.classList.remove('footer__contacts--opened')
+        footerContacts.classList.add('footer__contacts--closed')
   }
 })
 
