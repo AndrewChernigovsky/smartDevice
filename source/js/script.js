05b10601be699e7d6050376b1a5ec28151c34fba
UserRF <you@example.com>
2022-03-14 13:55:52 +0300

    1

--- source/js/script.js
+++ source/js/script.js
@@ -139,10 +139,40 @@ function isValidText(input) {
     return !/^\D{1,3}[А-Яа-яA-Za-z0-9_-]{10,150}$/.test(input.value);
 }
 
-formPopupPhone.oninput = function () {
-  var rep = /[a-zA-ZА-Яа-я]/g;
-  this.value = this.value.replace(rep, "")
-};
+window.addEventListener("DOMContentLoaded", function() {
+    function setCursorPosition(pos, elem) {
+        elem.focus();
+        if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+        else if (elem.createTextRange) {
+            var range = elem.createTextRange();
+            range.collapse(true);
+            range.moveEnd("character", pos);
+            range.moveStart("character", pos);
+            range.select()
+        }
+    }
+
+    function mask(event) {
+        var matrix = "+7 (___) ___ ____",
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = this.value.replace(/\D/g, "");
+        if (def.length >= val.length) val = def;
+        this.value = matrix.replace(/./g, function(a) {
+            return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+        });
+        if (event.type == "blur") {
+            if (this.value.length == 2) this.value = ""
+        } else setCursorPosition(this.value.length, this)
+    };
+        formPopupPhone.addEventListener("input", mask, false);
+        formPopupPhone.addEventListener("focus", mask, false);
+        formPopupPhone.addEventListener("blur", mask, false);
+
+        formPopupPhone.addEventListener("input", mask, false);
+        formPopupPhone.addEventListener("focus", mask, false);
+        formPopupPhone.addEventListener("blur", mask, false);
+    });
 
 
 // anchors
